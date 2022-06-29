b62cc9cd00eac3343d9b409bc2735449fbe3ba8b
Brenth Andrew J Miras <brenthmiras2@gmail.com>
2022-06-29 16:43:25 +0800

    GET /api/treasures - Add filter for prize

--- src/controllers/treasure/treasure.controller.js
+++ src/controllers/treasure/treasure.controller.js
@@ -2,15 +2,27 @@ import { Treasure, sequelize } from "../../models";
 import { successResponse, errorResponse, uniqueId } from "../../helpers";
 
 export const retrieve = async (req, res) => {
-  const { latitude, longitude, distance } = req.query;
+  
+  const { latitude, longitude, distance, prize } = req.query;
 
   try {
-    const query = `
+    const query = !prize ? `
       SELECT t.* FROM Treasures t
       WHERE ST_DISTANCE_SPHERE(
         POINT(t.longitude, t.latitude),
         POINT(:longitude, :latitude)
       ) / 1000 <= :distance
+    `
+    :
+    `
+      SELECT t.*, mv.* FROM Treasures t
+      LEFT JOIN MoneyValues mv
+      ON mv.treasure_id = t.id
+      WHERE ST_DISTANCE_SPHERE(
+        POINT(t.longitude, t.latitude),
+        POINT(:longitude, :latitude)
+      ) / 1000 <= :distance
+      AND mv.amt = :amount
     `;
 
     const queryOptions = {
@@ -20,6 +32,7 @@ export const retrieve = async (req, res) => {
         latitude,
         longitude,
         distance: Number(distance),
+        amount: prize,
       },
     };
 
--- src/controllers/treasure/treasure.validator.js
+++ src/controllers/treasure/treasure.validator.js
@@ -1,9 +1,13 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 export const retrieve = {
   query: {
     latitude: Joi.number().required(),
     longitude: Joi.number().required(),
     distance: Joi.allow(1, 10).required(),
+
+    // Accept $10 to $30
+    // Only whole numbers are accepted
+    prize: Joi.number().min(10).max(30),
   },
 };
